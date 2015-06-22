f2da2ff920f3e41c6b477daa00cca90cb93c947a
Chuck Houpt <chuck@habilis.net>
2015-06-22 14:13:09 +0000

    Split off CAS setup from main cassowary script.

--- cassowary.php
+++ cassowary.php
@@ -7,27 +7,8 @@
  * @license  http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
  */
 
-// Load the settings
-require_once 'config.php';
-
-// Load the CAS lib and any other dependencies
-require_once 'vendor/autoload.php';
-	
-// Enable debugging
-phpCAS::setDebug();
-
-// Initialize phpCAS
-phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
-
-// For production use set the CA certificate that is the issuer of the cert
-// on the CAS server and uncomment the line below
-// phpCAS::setCasServerCACert($cas_server_ca_cert_path);
-
-// For quick testing you can disable SSL validation of the CAS server.
-// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
-// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-phpCAS::setNoCasServerValidation();
-
+// Setup
+require_once 'cassowary-setup.php';
 
 // force CAS authentication, if not already authenticated
 
@@ -78,5 +59,3 @@ if ($file !== FALSE) {
 	http_response_code(404);
 	include('404.php');
 }
-
-?>
