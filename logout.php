8677e7accb092889eea29d0e6a93d2f9ca24684b
Chuck Houpt <chuck@habilis.net>
2015-07-22 13:39:38 +0000

    Replace use of SCRIPT_URI for OS X Server compatibility (mod_php environment).

--- logout.php
+++ logout.php
@@ -6,7 +6,9 @@ require_once '../cassowary/cassowary-setup.php';
 // logout if necessary
 
 if (phpCAS::isAuthenticated()) {
-	phpCAS::logoutWithRedirectService($_SERVER["SCRIPT_URI"]);
+	phpCAS::logoutWithRedirectService(
+		($_SERVER["'HTTPS'"] ? "https://" : "http://" )
+		. $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
 }
 ?>
 <!DOCTYPE html>
