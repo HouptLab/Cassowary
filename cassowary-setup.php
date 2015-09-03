36fc5177c68f72366101ca24cce41c4cbbbf6c5f
Chuck Houpt <chuck@habilis.net>
2015-09-03 14:15:33 +0000

    Adjusted Cassowary to check Certificate Authority certs, if defined and turned off debug logging.

--- cassowary-setup.php
+++ cassowary-setup.php
@@ -18,16 +18,18 @@ $cassowary_parent = $cassowary_parent == '/' ? '' : $cassowary_parent;
 require_once 'vendor/autoload.php';
 	
 // Enable debugging
-phpCAS::setDebug();
+//phpCAS::setDebug();
 
 // Initialize phpCAS
 phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
 
-// For production use set the CA certificate that is the issuer of the cert
-// on the CAS server and uncomment the line below
-// phpCAS::setCasServerCACert($cas_server_ca_cert_path);
-
-// For quick testing you can disable SSL validation of the CAS server.
-// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
-// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-phpCAS::setNoCasServerValidation();
+if (isset($cas_server_ca_cert_path)) {
+	// For production use set the CA certificate that is the issuer of the cert
+	// on the CAS server and uncomment the line below
+	phpCAS::setCasServerCACert($cas_server_ca_cert_path);
+} else {
+	// For quick testing you can disable SSL validation of the CAS server.
+	// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
+	// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
+	phpCAS::setNoCasServerValidation();
+}
