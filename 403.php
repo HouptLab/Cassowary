1ce073fb195de517b10e7880a5f9c2f4d7d33a9b
Chuck Houpt <chuck@habilis.net>
2015-09-04 18:21:54 +0000

    Added access-denied event tracking to help track down incorrect FSU-IDs (or hackers!).

--- 403.php
+++ 403.php
@@ -23,5 +23,21 @@
 		<p>Access to the requested URL <?php echo $_SERVER["REQUEST_URI"] ?>
 		   is forbidden for the user: <?php echo phpCAS::getUser() ?></p>
 		<p><a href='<?php echo $cassowary_parent ?>/logout/'>Logout</a></p>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackEvent', 'Error', 'AccessForbidden', '<?php echo phpCAS::getUser() ?>']);
+  //_paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.houptlab.org/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//piwik.houptlab.org/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 	</body>
 </html>
